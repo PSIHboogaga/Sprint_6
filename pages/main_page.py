f35839c394bb8f55037967b009a4fb25464b1645
PSIHboogaga <166983286+PSIHboogaga@users.noreply.github.com>
2024-08-27 22:49:05 +0500

    Add files via upload

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,6 @@
 import allure
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 from locators.main_page_locator import MainPageLocators
 from pages.base_page import BasePage
@@ -19,16 +21,27 @@ class MainPage(BasePage):
         logo_yandex = self.wait_and_find_element(MainPageLocators.LOGO_YANDEX)
         logo_yandex.click()
 
-    def open_faq(self, index):
-        faq_buttons = self.wait_and_find_element(*MainPageLocators.FAQ_BUTTONS)
-        faq_buttons[index].click()
+    def open_question(self, question_locator):
+        # Жмем на вопрос по переданному локатору
+        question_element = self.wait_and_find_element(question_locator)
+        # Прокручиваем к элементу
+        self.driver.execute_script("arguments[0].scrollIntoView();", question_element)
+        # Явно ждем, пока элемент станет кликабельным
+        WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable(question_element))
+        question_element.click()
 
-    def get_faq_answer(self, index):
-        faq_answers = self.wait_and_find_element(*MainPageLocators.FAQ_ANSWERS)
-        return faq_answers[index].text
-
-
-
+    def get_answer_text(self, answer_locator):
+        try:
+            # Явно ожидаем появления ответа
+            WebDriverWait(self.driver, 10).until(
+                expected_conditions.presence_of_element_located(answer_locator)
+            )
+            # Получаем элемент ответа по локатору
+            answer_element = self.wait_and_find_element(answer_locator)
+            return answer_element.text
+        except TimeoutException:
+            print("Ответ не появился в течение ожидаемого времени.")
+            return ""
 
 
 
--- pages/order_page.py
+++ pages/order_page.py
@@ -1,77 +1,79 @@
 import allure
-
 from locators.main_page_locator import MainPageLocators
 from locators.order_page_locator import OrderPageLocators
 from pages.base_page import BasePage
 
 
-
 class OrderPage(BasePage):
-    @allure.step("Клик кнопки заказать(вверху)")
+    @allure.step("Клик кнопки заказать (вверху)")
     def click_order_button_up(self):
-        order_button_up = self.wait_and_find_element(*MainPageLocators.BUTTON_ORDER_UP)
+        order_button_up = self.wait_and_find_element(MainPageLocators.BUTTON_ORDER_UP)
         order_button_up.click()
-    @allure.step('Заполняем поле имя')
+
+    @allure.step("Заполняем поле имя")
     def set_name_field(self, name):
-        name_field = self.wait_and_find_element(*OrderPageLocators.NAME_INPUT)
+        name_field = self.wait_and_find_element(OrderPageLocators.NAME_INPUT)
         name_field.send_keys(name)
-    @allure.step('Заполняем поле фамилия')
+
+    @allure.step("Заполняем поле фамилия")
     def set_surname_field(self, surname):
-        surname_field = self.wait_and_find_element(*OrderPageLocators.SURNAME_INPUT)
+        surname_field = self.wait_and_find_element(OrderPageLocators.SURNAME_INPUT)
         surname_field.send_keys(surname)
 
-    @allure.step('Заполняем поле адрес')
-    def set_adress_field(self, adress):
-        adress_field = self.wait_and_find_element(*OrderPageLocators.ADRESS_INPUT)
-        adress_field.send_keys(adress)
+    @allure.step("Заполняем поле адрес")
+    def set_address_field(self, address):
+        address_field = self.wait_and_find_element(OrderPageLocators.ADRESS_INPUT)
+        address_field.send_keys(address)
 
-    @allure.step('Заполняем поле телефон')
+    @allure.step("Заполняем поле телефон")
     def set_phone_field(self, phone):
-        phone_field = self.driver.find_element(*OrderPageLocators.PHONE_INPUT)
+        phone_field = self.wait_and_find_element(OrderPageLocators.PHONE_INPUT)
         phone_field.send_keys(phone)
-    @allure.step('Нажимаем на поле метро')
+
+    @allure.step("Нажимаем на поле метро")
     def click_metro_button(self):
-        metro_button = self.wait_and_find_element(*OrderPageLocators.METRO_INPUT)
+        metro_button = self.wait_and_find_element(OrderPageLocators.METRO_INPUT)
         metro_button.click()
 
-    @allure.step('Вводим Черкизовская')
+    @allure.step("Вводим станцию метро")
     def select_metro(self, metro):
-        metro = self.wait_and_find_element(*OrderPageLocators.METRO_INPUT)
-        metro.send_keys(metro)
+        metro_field = self.wait_and_find_element(OrderPageLocators.METRO_INPUT)
+        metro_field.send_keys(metro)
 
-    @allure.step('Нажимаем кнопку далее')
+    @allure.step("Нажимаем кнопку далее")
     def click_next_button(self):
-        next_button = self.wait_and_find_element(*OrderPageLocators.BUTTON_NEXT)
+        next_button = self.wait_and_find_element(OrderPageLocators.BUTTON_NEXT)
         next_button.click()
 
-    @allure.step('Нажимаем на поле когда привезти самокат')
+    @allure.step("Нажимаем на поле, когда привезти самокат")
     def click_time_field(self):
-        time_field = self.wait_and_find_element(*OrderPageLocators.TIME_FIELD)
+        time_field = self.wait_and_find_element(OrderPageLocators.TIME_FIELD)
         time_field.click()
 
-    @allure.step('Выбираем дату')
+    @allure.step("Выбираем дату")
     def click_time_field_select(self):
-        time_field_select = self.wait_and_find_element(*OrderPageLocators.TIME_FIELD_SELECT)
+        time_field_select = self.wait_and_find_element(OrderPageLocators.TIME_FIELD_SELECT)
         time_field_select.click()
 
-    @allure.step('Выбираем период')
+    @allure.step("Выбираем период аренды")
     def click_rental_period(self):
-        self.wait_and_find_element(*OrderPageLocators.RENTAL_PERIOD).click()
-        self.wait_and_find_element(*OrderPageLocators.RENTAL_PERIOD_SELECT).click()
+        self.wait_and_find_element(OrderPageLocators.RENTAL_PERIOD).click()
+        self.wait_and_find_element(OrderPageLocators.RENTAL_PERIOD_SELECT).click()
 
-    @allure.step('Нажимаем кнопку заказать')
+    @allure.step("Нажимаем кнопку заказать")
     def click_submit_button(self):
-        self.wait_and_find_element(*OrderPageLocators.SUBMIT_BUTTON).click()
+        self.wait_and_find_element(OrderPageLocators.SUBMIT_BUTTON).click()
 
-    @allure.step('Нажимаем кнопку да')
+    @allure.step("Нажимаем кнопку да на подтверждение")
     def click_yes_button(self):
-        self.wait_and_find_element(*OrderPageLocators.BUTTON_YES).click()
+        self.wait_and_find_element(OrderPageLocators.BUTTON_YES).click()
 
-    def order(self, name, surname, adress, phone, metro):
+    @allure.step("Оформляем заказ")
+    def order(self, name, surname, address, phone, metro):
         self.click_order_button_up()
         self.set_name_field(name)
         self.set_surname_field(surname)
-        self.set_adress_field(adress)
+        self.set_address_field(address)
         self.set_phone_field(phone)
         self.click_metro_button()
         self.select_metro(metro)
