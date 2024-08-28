f18fe3578e6aaa7b24b349190ea1e9315eae615a
PSIHboogaga <166983286+PSIHboogaga@users.noreply.github.com>
2024-08-29 00:12:59 +0500

    Add files via upload

---
+++ locators/main_page.py
@@ -0,0 +1,36 @@
+import allure
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+from locators.main_page_locator import MainPageLocators
+from pages.base_page import BasePage
+
+class MainPage(BasePage):
+    @allure.step('Клик кнопки заказать(вверху\внизу)')
+    def click_order_button(self, index):
+        order_button = MainPageLocators.BUTTON_ORDER_UP if index == 0 else MainPageLocators.BUTTON_ORDER_DOWN
+        self.wait_and_find_element(order_button).click()
+
+    @allure.step('Клик на логотип «Самоката»')
+    def click_logo_scooter(self):
+        logo_scooter = self.wait_and_find_element(MainPageLocators.LOGO_SCOOTER)
+        logo_scooter.click()
+
+    @allure.step('Клик на логотип «Яндекс»')
+    def click_logo_yandex(self):
+        logo_yandex = self.wait_and_find_element(MainPageLocators.LOGO_YANDEX)
+        logo_yandex.click()
+
+    @allure.step('Клик на вопрос')
+    def open_question(self, question_locator):
+
+        question_element = self.wait_and_find_element(question_locator)
+        self.driver.execute_script("arguments[0].scrollIntoView();", question_element)  # Прокручиваем к элементу
+        self.wait_and_find_element(question_locator)
+        question_element.click()
+
+    @allure.step('получаем ответ')
+    def get_answer_text(self, answer_locator):
+        self.wait_for_element_to_appear(answer_locator)  # Ожидаем появления ответа
+        answer_element = self.wait_and_find_element(answer_locator)  # Получаем элемент ответа по локатору
+        return answer_element.text
